49dbab4a1204ee855eb576d0d657d6933e2aab4d
aelhosiny <amr.elhosiny@si-vision.com>
2014-10-16 14:14:32 +0200

    Added synthesis scripts

---
+++ scripts/synthesis/synth.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+## Clean old compilation results
+## Keep the reports only
+
+toplevel=$1
+
+export ROOT_DIR=`echo $PWD | sed 's_/scripts/synthesis__'`
+echo "root dir is $ROOT_DIR"
+export scr_dir=$ROOT_DIR/scripts/synthesis
+export synth_dir=$ROOT_DIR/synthesis/$toplevel
+
+mkdir -p $synth_dir
+cd $synth_dir
+
+xst -ifn $scr_dir/settings.xst -ofn $toplevel".srp"
+
+
+
+  echo "===============================================" > $synth_dir/Warnings.txt
+  echo "============ Unused signals report ============" >> $synth_dir/Warnings.txt
+  echo "===============================================" >> $synth_dir/Warnings.txt
+  
+  grep "since the identifier is never used" $toplevel".srp" | grep --color "WARNING" >> $synth_dir/Warnings.txt
+  
+  echo "============================================" >>  $synth_dir/Warnings.txt
+  echo "============ Unused input ports ============" >> $synth_dir/Warnings.txt
+  echo "============================================" >> $synth_dir/Warnings.txt
+  
+  grep "This port will be preserved and left unconnected" ${toplevel}.srp | grep "Input"  | grep --color "WARNING"  >> $synth_dir/Warnings.txt
+  
+  
+  echo "==========================================" >>  $synth_dir/Warnings.txt
+  echo "============ Undriven Signals ============" >> $synth_dir/Warnings.txt
+  echo "==========================================" >> $synth_dir/Warnings.txt
+  
+  grep "WARNING" ${toplevel}.srp | grep "does not have a driver" >> $synth_dir/Warnings.txt
+  
+  #### Get error Report
+  
+  grep "ERROR" ${toplevel}.srp > $synth_dir/errors.txt
+  
+  
+  #### Get all Warnings
+  
+  grep "WARNING" ${toplevel}.srp > $synth_dir/all_warnings.txt
+
